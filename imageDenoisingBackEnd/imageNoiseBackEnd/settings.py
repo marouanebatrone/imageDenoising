9677ecb6ac65d425028378156e34c7d59e9500c5
marouane batrone <batrone.marouane@gmail.com>
2024-05-30 18:05:46 +0100

    back end changes

--- imageDenoisingBackEnd/imageNoiseBackEnd/settings.py
+++ imageDenoisingBackEnd/imageNoiseBackEnd/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'image_denoising',
         'USER': 'root', 
-        'PASSWORD': '',
+        'PASSWORD': 'marouane913.',
         'HOST': 'localhost',  # Optional
         'PORT': '3306',        # Optional
     }
